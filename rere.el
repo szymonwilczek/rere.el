47ac7c973f55236e84be4557568f3704c9ea8020
Szymon Wilczek <swilczek.lx@gmail.com>
2026-09-18 22:44:14 +0200

    metadata: update package summary to adhere to MELPA standards
    Remove redundant 'for GNU Emacs' from the package header summary line
    to eliminate package-lint warning and follow MELPA conventions.
    
    Signed-off-by: Szymon Wilczek <swilczek.lx@gmail.com>

--- rere.el
+++ rere.el
@@ -1,4 +1,4 @@
-;;; rere.el --- Rebase Review for GNU Emacs  -*- lexical-binding: t; -*-
+;;; rere.el --- Review git rebase diffs line by line  -*- lexical-binding: t; -*-
 
 ;; Copyright (C) 2026 Szymon Wilczek
 
